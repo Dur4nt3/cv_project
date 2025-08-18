d37e99a5ab8744fb1b8ba996fe6315d472f36009
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-08-18 15:16:26 +0300

    Update "Experience" form section

--- src/modules/components/ExperienceSegment.jsx
+++ src/modules/components/ExperienceSegment.jsx
@@ -3,5 +3,53 @@ export default function ExperienceSegment({
     experienceItem,
     updateExperience,
 }) {
-    return <div className='experience-segment'></div>;
+    return (
+        <div className='experience-segment' id={itemId}>
+            {Object.keys(experienceItem).map((key) => {
+                if (key === 'positionDescription') {
+                    return <span key={key}>Placeholder for description</span>;
+                }
+
+                const labels = {
+                    position: 'Position',
+                    companyName: 'Company Name',
+                    companyLocation: 'Company Location',
+                    positionStartDate: 'Position Start Date',
+                    positionEndDate: 'Position End Date',
+                };
+
+                const placeholders = {
+                    position: 'E.g., Fullstack Web Developer',
+                    companyName: 'E.g., Example LTD',
+                    companyLocation: 'E.g., Austin, Texas',
+                    positionStartDate: 'E.g., 30-12-2023',
+                    positionEndDate: 'E.g., 30-12-2024',
+                };
+
+                return (
+                    <div className='form-row' key={key}>
+                        <label className='form-label'>
+                            {labels[key]}
+                            <span className='required-indicator'>*</span>
+                        </label>
+                        <input
+                            className='form-input'
+                            type={
+                                key === 'positionStartDate' ||
+                                key === 'positionEndDate'
+                                    ? 'date'
+                                    : 'text'
+                            }
+                            required={true}
+                            value={experienceItem[key]}
+                            onChange={(event) =>
+                                updateExperience(event, itemId, key)
+                            }
+                            placeholder={placeholders[key]}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
